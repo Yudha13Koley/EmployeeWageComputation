90b476766ab0bae93728ff294513a2f80912aa33
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-22 16:25:43 +0530

    UC2 done

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -2,13 +2,20 @@ package com.employee;
 
 public class EmployeeWage {	
 	public static void main (String[] args) {
-		int Is_Full_time = 0 ;
+		int Is_Full_time = 1 ;
+		int Emp_rate_per_hour= 20;
+		int emphours=0;
+		int empwage=0;
 		double empcheck =Math.floor(Math.random()*10)%2 ;
 		if(empcheck==Is_Full_time)
-			System.out.println("Employee is Present !");
-		else
+		{ emphours=8;
+		empwage=emphours*Emp_rate_per_hour;
+			System.out.println("Emp Wage :" +empwage);
+		}
+		else {
 			System.out.println("Employee is Absent !");
+		System.out.println("Emp Wage :" +empwage);
+		}
 
-}
-
+	}
 }
