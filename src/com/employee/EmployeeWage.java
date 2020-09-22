341461cbb8b62773ff54a3aa99dc788b972c6e7e
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-22 16:19:48 +0530

    UC1 done

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -1,8 +1,13 @@
 package com.employee;
 
 public class EmployeeWage {	
-	public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation Program !");
+	public static void main (String[] args) {
+		int Is_Full_time = 0 ;
+		double empcheck =Math.floor(Math.random()*10)%2 ;
+		if(empcheck==Is_Full_time)
+			System.out.println("Employee is Present !");
+		else
+			System.out.println("Employee is Absent !");
 
 }
 
