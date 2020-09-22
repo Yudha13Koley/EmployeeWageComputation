fcf88948bd18ce0e10755fd9cf5f416062132758
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-22 16:38:02 +0530

    UC5 done

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -4,9 +4,10 @@ public class EmployeeWage {
 	public static final int Is_Full_Time=2;
 	public static final int Is_Part_Time=1;
 	public static final int Emp_rate_per_hour=20;
+	public static final int Working_Days_Per_Month=20;
 		public static void main(String[] args) {
-			int emphrs=0;
-			int empwage=0;
+			int emphrs=0,empwage=0,Total=0;
+			for(int i=0;i < Working_Days_Per_Month;i++) {
 			double empcheck =Math.floor(Math.random()*10)% 3;
 			switch((int)empcheck) {
 			case Is_Part_Time :emphrs=4;
@@ -16,6 +17,9 @@ public class EmployeeWage {
 			default: emphrs=0;
 			}
 			empwage=emphrs*Emp_rate_per_hour;
+			Total+=empwage;
 			System.out.println("Emp Wage :" +empwage);
-}
+			}
+			System.out.println("Total Emp Wage :" +Total);
+			}
 }
