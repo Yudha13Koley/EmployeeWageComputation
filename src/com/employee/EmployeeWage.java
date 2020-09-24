f4151ee4c69a064fbd0edb238ed2631185d3d03e
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-24 16:24:19 +0530

    UC9 done

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -3,8 +3,21 @@ package com.employee;
 public class EmployeeWage {	
 	public static final int Is_Full_Time=2;
 	public static final int Is_Part_Time=1;
+
+	private final String company;
+	private final int empRatePerHour;
+	private final int workingDaysPerMonth;
+	private final int maxHoursWorking;
+	private int Total;
 	
-	public static int computeWage(String company,int empRatePerHour,int workingDaysPerMonth,int maxHoursWorking ) {
+	public EmployeeWage(String company,int empRatePerHour,int workingDaysPerMonth,int maxHoursWorking) {
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.workingDaysPerMonth=workingDaysPerMonth;
+		this.maxHoursWorking=maxHoursWorking;
+	}
+	
+	public void computeWage() {
 		int TotalHours=0,TotalDays=0;
 		while(TotalHours <= maxHoursWorking && TotalDays<workingDaysPerMonth) {
 			int emphrs=0;
@@ -20,13 +33,19 @@ public class EmployeeWage {
 		TotalHours+=emphrs;
 		System.out.println("Emp Hours :" +emphrs+" In Day :"+TotalDays);
 		}
-		int Total=TotalHours*empRatePerHour;
-		System.out.println("Total Emp Wage :" +Total+" In Total Hours :"+TotalHours);
-		return Total;
+		Total=TotalHours*empRatePerHour;
 		}
+	@Override
+	public String toString() {	
+		return "Total Employee wage for company " +company+ " is : " +Total;
+	}
 		public static void main(String[] args) {
-		computeWage("DMart",20,20,100);
-		computeWage("Reliance",10,22,120);
+		EmployeeWage DMart=new EmployeeWage("DMart",20,20,100);
+		EmployeeWage Reliance=new EmployeeWage("Reliance",15,22,100);
+		DMart.computeWage();
+		System.out.println(DMart);
+		Reliance.computeWage();
+		System.out.println(Reliance);
          
 }
 }
